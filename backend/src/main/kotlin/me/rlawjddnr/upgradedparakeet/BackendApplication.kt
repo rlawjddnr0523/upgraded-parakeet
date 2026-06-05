dc521631b590df588d2f466c473e21262fe5d340
김정욱 <yeoback07@icloud.com>
2026-06-05 23:31:52 +0900

    feat: add backend

---
+++ backend/src/main/kotlin/me/rlawjddnr/upgradedparakeet/BackendApplication.kt
@@ -0,0 +1,11 @@
+package me.rlawjddnr.upgradedparakeet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/me/rlawjddnr/upgradedparakeet/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.rlawjddnr.upgradedparakeet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
